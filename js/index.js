f2100e9ae3292b13293d1dc92f4b25149142c371
rock-zion <rock_zion@outlook.com>
2021-02-23 23:29:05 +0100

    render the list of catnames

--- js/index.js
+++ js/index.js
@@ -1,34 +1,66 @@
 let myCats = [
-    {
-      name: "Shelly",
-      img: "../img/1.jpg",
-      count: 0,
-    },
-    {
-      name: "Deerbra",
-      img: "../img/2.jpg",
-      count: 0,
-    },
-    {
-      name: "Nelly",
-      img: "../img/3.jpg",
-      count: 0,
-    },
-    {
-      name: "Jolly",
-      img: "../img/4.jpg",
-      count: 0,
-    },
-    {
-      name: "Smiley",
-      img: "../img/5.jpg",
-      count: 0,
-    },
-    {
-      name: "Carly",
-      img: "../img/6.jpg",
-      count: 0,
-    },
-  ];
+  {
+    name: "Shelly",
+    img:
+      "https://images.unsplash.com/photo-1577023311546-cdc07a8454d9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Y2F0c3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    count: 0,
+  },
+  {
+    name: "Deerbra",
+    img:
+      "https://images.unsplash.com/photo-1574158622682-e40e69881006?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80",
+    count: 0,
+  },
+  {
+    name: "Nelly",
+    img:
+      "https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    count: 0,
+  },
+  {
+    name: "Jolly",
+    img:
+      "https://images.unsplash.com/photo-1583795128727-6ec3642408f8?ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    count: 0,
+  },
+  {
+    name: "Smiley",
+    img:
+      "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGNhdHN8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60",
+    count: 0,
+  },
+  {
+    name: "Carly",
+    img:
+      "https://images.unsplash.com/photo-1574231164645-d6f0e8553590?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1335&q=80",
+    count: 0,
+  },
+];
 
-  
+let catList = document.querySelector("#cat-list");
+let catCount = document.querySelector("#cat-count");
+let catImg = document.querySelector("#cat-img");
+let catPrompt = document.querySelector("#prompt");
+let currentCat = myCats[0];
+
+for (let index = 0; index < myCats.length; index++) {
+  //create list items and add name to content
+  let cat = myCats[index];
+  let catItem = document.createElement("li");
+  catItem.innerHTML = myCats[index].name;
+
+  catItem.addEventListener(
+    "click",
+    (function (catCopy) {
+      return function () {
+        catImg.src = catCopy.img;
+        catCopy.textContent = ` has been clicked ${catCopy.count}`;
+      };
+    })(cat)
+  );
+
+  catList.appendChild(catItem);
+}
+
+catImg.src = currentCat.img;
+catCount.textContent = `${currentCat.name} has been clicked ${currentCat.count} times`;
