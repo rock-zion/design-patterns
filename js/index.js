264e7d5b0b09dd5cc95c45fa7de066b31e8e6a9d
rock-zion <rock_zion@outlook.com>
2021-02-23 20:21:56 +0100

    added an array of cats

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let myCats = [
+    {
+      name: "Shelly",
+      img: "../img/1.jpg",
+      count: 0,
+    },
+    {
+      name: "Deerbra",
+      img: "../img/2.jpg",
+      count: 0,
+    },
+    {
+      name: "Nelly",
+      img: "../img/3.jpg",
+      count: 0,
+    },
+    {
+      name: "Jolly",
+      img: "../img/4.jpg",
+      count: 0,
+    },
+    {
+      name: "Smiley",
+      img: "../img/5.jpg",
+      count: 0,
+    },
+    {
+      name: "Carly",
+      img: "../img/6.jpg",
+      count: 0,
+    },
+  ];
+
+  
