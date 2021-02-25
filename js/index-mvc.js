6e5d5ab4e5a11f1ea962ba712d405762ae31a511
rock-zion <rock_zion@outlook.com>
2021-02-25 10:18:23 +0100

    rendered the list of cats and initialize image, name and click count of first cat

--- js/index-mvc.js
+++ js/index-mvc.js
@@ -25,8 +25,33 @@ let controller = {
 /* >>> VIEW <<< */
 let view = {
   init: function () {
+    this.catList = document.querySelector("#cat-list");
+    this.catCount = document.querySelector("#cat-count");
+    this.catImg = document.querySelector("#cat-img");
+    this.catPrompt = document.querySelector("#prompt");
+
     this.render();
+    this.selectCat();
   },
-  render: function () {},
+  selectCat: function () {
+    let currentCat = model.currentCat;
+    this.catImg.src = currentCat.img;
+    this.catPrompt = `click on ${currentCat.name}'s picture to increase the count`;
+    this.catCount.innerHTML = `<span id="bold">${currentCat.name} &nbsp; </span> has been clicked &nbsp; <span id="bold">${currentCat.count}</span> &nbsp; times`;
+  },
+
+  render: function () {
+    let cats = controller.getCats();
+    for (var i = 0; i < cats.length; i++) {
+      let catItem = document.createElement("li");
+      catItem.style.cursor = "pointer";
+      let cat = cats[i];
+      catItem.textContent = cat.name;
+      catItem.addEventListener("click", function () {
+        controller.setCurrentCat(cat);
+      });
+      this.catList.appendChild(catItem);
+    }
+  },
 };
 controller.init();
