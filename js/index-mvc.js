4a34135e261c1f1574e39b0c0b7df1c9d580edf8
rock-zion <rock_zion@outlook.com>
2021-02-25 09:26:34 +0100

    initialize controller

--- js/index-mvc.js
+++ js/index-mvc.js
@@ -1,11 +1,27 @@
 import { myCats } from "../js/model.js";
 
 /* >>> MODEL <<< */
-let modal = {
+let model = {
   currentCat: null,
   myCats,
 };
 
 /* >>> CONTROLLER <<< */
+let controller = {
+  init: function () {
+    model.currentCat = this.getCats()[0];
+    this.render();
+  },
+
+  getCats: function () {
+    return model.myCats;
+  },
+
+  render: function () {
+    view.init();
+  },
+};
 
 /* >>> VIEW <<< */
+
+controller.init();
